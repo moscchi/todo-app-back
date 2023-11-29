905820d4de330ec2b46cac8cc4413d9ff941bedc
Moschi <smoschini@kovix-group.com>
2023-11-29 19:11:07 -0300

    Feat: Login endpoint

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,24 @@
 const bcrypt =require('bcryptjs')
-const UserModel = require('../models/users.models')
+const UserModel = require('../models/users.models');
+const generateJWT = require('../utils/generateJWT');
+
+const loginService = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await UserModel.findOne({ username });
+        if(!user) return res.status(400).json({ message: 'Username or password incorrect' });
+
+        const isValidPassword = bcrypt.compareSync(password, user.password);
+        if(!isValidPassword) return res.status(400).json({ message: 'Username or password incorrect' });
+
+        const token = generateJWT(user.username);
+        return res.status(200).json({ message: 'User logged successfully', token });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
 const registerService = async (req, res) => {
     try {
         const { username, password} = req.body;
@@ -13,4 +32,7 @@ const registerService = async (req, res) => {
     }
 }
 
-module.exports = registerService;
+module.exports = {
+    registerService,
+    loginService
+};
